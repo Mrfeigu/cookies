e0d0a9f5195fa99efa552b73637883ac5d472b97
linzhenghui <1015583245@qq.com>
2020-07-17 20:27:59 +0800

    leecode：接雨水

--- exercise/src/main/java/com/lc/exercise/hard/exercise2.java
+++ exercise/src/main/java/com/lc/exercise/hard/exercise2.java
@@ -1,28 +1,57 @@
 package com.lc.exercise.hard;
 
-
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-
 public class exercise2 {
 
+    /**
+     * 中心开花boom boom boom
+     * @param height
+     * @return
+     */
     public int trap(int[] height) {
-        LinkedList<Integer> heightList = new LinkedList<>();
-        for (int i : height) heightList.add(i);
-        int waterSum = 0, tempWaterSum = 0, leftPillar = 0;
-        for(int i = 0; i < heightList.size(); i++) {
-
+        int res = 0;
+        for (int i = 1; i < height.length - 1; i++) {
+            int maxLeft = 0, maxRight = 0;
+            for (int j = i; j >= 0 ; j--) {
+                maxLeft = Math.max(maxLeft, height[j]);
+            }
+            for (int j = i; j < height.length; j++) {
+                maxRight = Math.max(maxRight, height[j]);
+            }
+            res += Math.min(maxLeft, maxRight) - height[i];
         }
-        return waterSum;
+        return res;
+    }
+
+    /**
+     * 动态规划？！思想还是上面的思想
+     * @param height
+     * @return
+     */
+    public int trap2(int[] height) {
+        if(height.length < 1) return 0;
+        int[] leftMax = new int[height.length];
+        int[] rightMax = new int[height.length];
+        int res = 0;
+        leftMax[0] = height[0];
+        rightMax[height.length - 1] = height[height.length - 1];
+        for (int i = 1; i < height.length; i++) {
+            leftMax[i] = Math.max(height[i], leftMax[i-1]);
+        }
+        for (int i = height.length - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(height[i], rightMax[i+1]);
+        }
+        for (int i = 1; i < height.length - 1; i++) {
+            res += (Math.min(leftMax[i] ,rightMax[i]) - height[i]);
+        }
+        return res;
     }
 
 
 
-
     public static void main(String[] args){
-        int[] arr = {0,1,0,2,1,0,1,3,2,1,2,1};
-        int trap = new exercise2().trap(arr);
+        int[] arr = {2,0,2};
+        int trap = new exercise2().trap2(arr);
+        System.out.println(trap);
         System.out.println("ending...");
     }
 
