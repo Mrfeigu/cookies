693f358051a28a19529da733e2f2634a8a077f19
linzhenghui <1015583245@qq.com>
2020-07-21 21:05:13 +0800

    no message

--- exercise/src/main/java/com/lc/exercise/hard/exercise3.java
+++ exercise/src/main/java/com/lc/exercise/hard/exercise3.java
@@ -4,13 +4,14 @@ package com.lc.exercise.hard;
 public class exercise3 {
 
     /**
-     * 通配符匹配 todo
+     * 通配符匹配
      * @param s
      * @param p
      * @return
      */
     public boolean isMatch(String s, String p) {
         if("*".equals(p)) return true;
+        if("".equals(p) && "".equals(s)) return true;
         if("".equals(p) || "".equals(s)) return false;
         boolean[][] dp = new boolean[p.length() + 1][s.length() + 1];
         // 初始化动态数组
@@ -23,25 +24,28 @@ public class exercise3 {
             dp[0][i] = true;
         }
         for (int i = 1; i <= p.length(); i++) {
-            int q = i;
             for (int j = 1; j <= s.length(); j++) {
                 if(dp[i-1][j-1]){
                     if(p.charAt(i-1) == '*') {
-                        dp[i][j] = true;
+                        while(j <= s.length()){
+                            dp[i][j] = true;
+                            j++;
+                        }
+                        break;
                     } else if(s.charAt(j-1) == p.charAt(i-1) || p.charAt(i-1) == '?'){
                         dp[i][j] = true;
-                        i++;
-                        if(i > p.length()) break;
+                        if(!(i-2 >= 0 && p.charAt(i-2) == '*')) break;
                     }
-                }else{ dp[i][j] = false; }
+                } else{
+                    dp[i][j] = false;
+                }
             }
-            i = q;
         }
         return dp[p.length()][s.length()];
     }
 
     public static void main(String[] args){
-        boolean match = new exercise3().isMatch("adceb", "*a*b");
+        boolean match = new exercise3().isMatch("abefcdgiescdfimde", "ab*cd?i*de");
         System.out.println("ending...");
     }
 
