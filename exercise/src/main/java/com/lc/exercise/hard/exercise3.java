51eeb2a8d7a1884a04f2fe60b039657659deeeec
linzhenghui <1015583245@qq.com>
2020-07-22 10:35:33 +0800

    LeetCode，通配符匹配

--- exercise/src/main/java/com/lc/exercise/hard/exercise3.java
+++ exercise/src/main/java/com/lc/exercise/hard/exercise3.java
@@ -5,12 +5,12 @@ public class exercise3 {
 
     /**
      * 通配符匹配
-     *
+     * 有用例过不完
      * @param s
      * @param p
      * @return
      */
-    public boolean isMatch(String s, String p) {
+    public boolean isMatch2(String s, String p) {
         if ("*".equals(p)) return true;
         if ("".equals(p) && "".equals(s)) return true;
         if ("".equals(p) || "".equals(s)) return false;
@@ -43,8 +43,41 @@ public class exercise3 {
         return dp[p.length()][s.length()];
     }
 
+    /**
+     * 官方题解
+     * @param s
+     * @param p
+     * @return
+     */
+    public boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        for (int i = 1; i <= n; ++i) {
+            if (p.charAt(i - 1) == '*') {
+                dp[0][i] = true;
+            } else {
+                break;
+            }
+        }
+        // 匹配串
+        for (int i = 1; i <= m; ++i) {
+            // 模式串
+            for (int j = 1; j <= n; ++j) {
+                if (p.charAt(j - 1) == '*') {
+                    // 这个就很有灵性
+                    dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+                } else if (p.charAt(j - 1) == '?' || s.charAt(i - 1) == p.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
     public static void main(String[] args) {
-        boolean match = new exercise3().isMatch("cabab", "*ab");
+        boolean match = new exercise3().isMatch("a", "a*a");
         System.out.println("ending...");
     }
 
