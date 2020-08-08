b1f4176081868ce23fd027c194e20b67a1468965
Mrfeigu <1079574682@qq.com>
2020-08-08 11:30:42 +0800

    增加测试工具

--- spring/src/main/java/com/delicacy/cookies/service/TestService.java
+++ spring/src/main/java/com/delicacy/cookies/service/TestService.java
@@ -1,12 +1,29 @@
 package com.delicacy.cookies.service;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class TestService {
 
+    public String userName = "张三";
+
     public Object testStr(String var) {
         return var;
     }
+
+
+
+    public static void main(String[] args){
+        AnnotationConfigApplicationContext acac = new AnnotationConfigApplicationContext();
+        acac.registerBean(TestService.class);
+        acac.refresh();
+        TestService bean = acac.getBean(TestService.class);
+        System.out.println(bean.userName);
+    }
+
+
 }
 
