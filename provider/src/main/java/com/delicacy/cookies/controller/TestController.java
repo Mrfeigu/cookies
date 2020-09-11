510f82218169142d2f3f129a2a86a7f33c7df09c
linzhenghui <1015583245@qq.com>
2020-09-11 20:41:32 +0800

    RabbitMQ的demo

--- provider/src/main/java/com/delicacy/cookies/controller/TestController.java
+++ provider/src/main/java/com/delicacy/cookies/controller/TestController.java
@@ -2,21 +2,24 @@ package com.delicacy.cookies.controller;
 
 import com.delicacy.cookies.ioc.service.TargetService;
 import com.delicacy.cookies.ioc.service.UserInfoService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.LinkedList;
+import java.util.List;
 
+@Slf4j
 @DependsOn("targetService2")
 @RestController
 @RequestMapping("/ahh")
 public class TestController {
-
-
-    @Resource(name = "targetService3")
-    private TargetService targetService;
 
     @Resource
     private UserInfoService userInfoService;
@@ -28,8 +31,12 @@ public class TestController {
 
     @GetMapping("/testTarget")
     public Object testTarget(){
-        Object o = userInfoService.sayHello();
-        return targetService.printlnMsg();
+        List<Integer> list = new LinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        log.info("TestController#  list:{}", list);
+        return null;
     }
 
 
---
+++ rabbitmq/src/main/java/com/delicacy/cookies/config/QueueConfig.java
@@ -0,0 +1,22 @@
+package com.delicacy.cookies.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author linzhenghui
+ * @date 2020/9/11
+ */
+@Slf4j
+@Component
+public class QueueConfig {
+
+    @Bean(name = "monitorReportReduceQueue")
+    Queue queue() {
+        return new Queue("monitor_report_reduce_queue", false);
+    }
+
+
+}
