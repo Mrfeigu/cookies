52c2860c51c3ea05b7d4548ec48a44fddaf6d70d
Mrfeigu <1079574682@qq.com>
2020-09-22 00:09:18 +0800

    增加redisTemplate的使用

--- redis/src/main/java/com/delicacy/cookies/RedisDemo.java
+++ redis/src/main/java/com/delicacy/cookies/RedisDemo.java
@@ -1,6 +1,14 @@
 package com.delicacy.cookies;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.io.Serializable;
 
 /**
  * @author linzhenghui
@@ -9,9 +17,26 @@ import org.springframework.stereotype.Service;
 @Service
 public class RedisDemo {
 
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
+
     public Object test(){
-        System.out.println("早上好");
-        return "早上好";
+        TestRedisDemoEntity testRedisDemoEntity = new TestRedisDemoEntity();
+        testRedisDemoEntity.setUserId(1);
+        testRedisDemoEntity.setAge(1);
+        testRedisDemoEntity.setUserName("feigu");
+        redisTemplate.opsForValue().set("ahhh", testRedisDemoEntity);
+        return redisTemplate.opsForValue().get("ahhh");
+    }
+
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor(staticName = "of")
+    private static class TestRedisDemoEntity implements Serializable {
+        private Integer userId;
+        private Integer age;
+        private String userName;
     }
 
 }
--- redis/src/main/java/com/delicacy/cookies/config/RedisConfig.java
+++ redis/src/main/java/com/delicacy/cookies/config/RedisConfig.java
@@ -1,25 +1,47 @@
 package com.delicacy.cookies.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.DefaultBaseTypeLimitingValidator;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
  * @author linzhenghui
  * @date 2020/9/16
  */
-//@Configuration
+@Configuration
 public class RedisConfig {
 
     @Bean
-    public RedisConnectionFactory getRedisConnectionFactory(){
-        // todo RedisConnectionFactory这个Bean的继承是哪一个呢
-
-        return null;
+    // @ConditionalOnMissingBean(name = "redisTemplate")
+    public RedisTemplate<String, Object> buildRedisTemplate(RedisConnectionFactory factory){
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        // 设置工厂
+        redisTemplate.setConnectionFactory(factory);
+        // value 设置序列化
+        Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.activateDefaultTyping(new DefaultBaseTypeLimitingValidator());
+        serializer.setObjectMapper(om);
+        redisTemplate.setValueSerializer(serializer);
+        // key 设置序列化
+        StringRedisSerializer stringSerializer = new StringRedisSerializer();
+        redisTemplate.setKeySerializer(stringSerializer);
+        // hash 的key也采用String的序列化方式
+        redisTemplate.setHashKeySerializer(stringSerializer);
+        // hash 的value设置序列化
+        redisTemplate.setHashValueSerializer(serializer);
+        //
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
     }
-
-    
-
 
 }
