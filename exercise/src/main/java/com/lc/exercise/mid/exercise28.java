8016c882af2d1fd2124f7e1c51af16ea2ed1f7e0
Mrfeigu <1079574682@qq.com>
2020-10-08 19:30:16 +0800

    增加LeetCode的55.跳跃游戏题解

--- exercise/src/main/java/com/lc/exercise/mid/exercise28.java
+++ exercise/src/main/java/com/lc/exercise/mid/exercise28.java
@@ -3,6 +3,11 @@ package com.lc.exercise.mid;
 import java.util.*;
 
 /**
+ * 这题不会.....好丢人
+ *
+ * 按照贪心的思想，就是每次保持最远可到达
+ *
+ *
  * 给定一个非负整数数组，你最初位于数组的第一个位置。
  * 数组中的每个元素代表你在该位置可以跳跃的最大长度。
  * @author linzhenghui
@@ -11,27 +16,17 @@ import java.util.*;
 public class exercise28 {
 
     public boolean canJump(int[] nums) {
-        int size = nums.length;
-        int index = 0;
-        LinkedList<Integer> list = new LinkedList<>();
+        int maxJump = 0;
         for (int i = 0; i < nums.length; i++) {
-
-            for (int j = index + 1; j <= index + nums[index]; j++) {
-                list.add(nums[j]);
-            }
-            list.sort(Comparator.comparingInt(o->o));
-
+            if(i > maxJump) return false;
+            if(i + nums[i] > maxJump) maxJump = nums[i] + i;
         }
-
-
-
-
-
-        return false;
+        return true;
     }
 
     public static void main(String[] args){
-
+        int []nums = {3,2,1,0,4};
+        System.out.println(new exercise28().canJump(nums));
     }
 
 }
