8b5905060e85e74efaf97ae458ad718d9675073f
Mrfeigu <1079574682@qq.com>
2020-10-17 11:21:40 +0800

    增加LeetCode56：合并区间

--- exercise/src/main/java/com/lc/exercise/mid/exercise29.java
+++ exercise/src/main/java/com/lc/exercise/mid/exercise29.java
@@ -1,7 +1,6 @@
 package com.lc.exercise.mid;
 
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
 
 /**
  * 给出一个区间的集合，请合并所有重叠的区间。
@@ -10,19 +9,29 @@ import java.util.Comparator;
  */
 public class exercise29 {
     public static int[][] merge(int[][] intervals) {
-
+        // 这个题目的精髓就在于这个排序了
         Arrays.sort(intervals, Comparator.comparingInt(o -> o[0]));
+        if(intervals.length < 1){
+            return new int[0][];
+        }
+        // 这个数据结构也比较方便
+        LinkedList<int[]> list = new LinkedList<>();
+        list.add(intervals[0]);
 
         for (int i = 1; i < intervals.length; i++) {
-            // todo
-
-
-
-
+            int[] pop = list.getLast();
+            int[] interval = intervals[i];
+            if(pop[0] == interval[0] || pop[0] < interval[0] && interval[0] <= pop[1]){
+                pop[1] = Math.max(pop[1], interval[1]);
+            }else {
+                list.add(interval);
+            }
         }
-
-
-        return null;
+        int[][] res = new int[list.size()][];
+        for (int i = 0; i < list.size(); i++) {
+            res[i] = list.get(i);
+        }
+        return res;
     }
 
     public static void main(String[] args){
