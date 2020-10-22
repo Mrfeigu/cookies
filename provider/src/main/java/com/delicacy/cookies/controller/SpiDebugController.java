7da0027fef374c97a933143754087471cc4b021f
linzhenghui <1015583245@qq.com>
2020-10-22 20:40:28 +0800

    增加spi demo测试

---
+++ provider/src/main/java/com/delicacy/cookies/controller/SpiDebugController.java
@@ -0,0 +1,31 @@
+package com.delicacy.cookies.controller;
+
+import com.delicacy.cookies.spi.Robot;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ServiceLoader;
+
+/**
+ * @author linzhenghui
+ * @date 2020/10/22
+ */
+
+
+@RestController
+@RequestMapping("/SpiDebug")
+public class SpiDebugController {
+
+    @GetMapping("/test")
+    public Object test(){
+        // 所以我还没搞懂spi有啥用，还是一样要重新编译，并不能运行时加载
+        ServiceLoader<Robot> serviceLoader = ServiceLoader.load(Robot.class);
+        serviceLoader.reload();
+        serviceLoader.forEach(Robot::sayHello);
+        return "success";
+    }
+
+
+
+}
