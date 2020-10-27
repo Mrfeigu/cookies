25719336047a4001824afa5ee722c41024decc5b
Mrfeigu <1079574682@qq.com>
2020-10-27 23:17:53 +0800

    增加策略模式包

---
+++ design-mode/src/main/java/com/delicacy/cookies/strategy/test.java
@@ -0,0 +1,15 @@
+package com.delicacy.cookies.strategy;
+
+/**
+ * @author feigu
+ * @date 2020/10/24
+ */
+public class test {
+
+    public static void main(String[] args) {
+
+        System.out.println("ending...");
+    }
+
+
+}
