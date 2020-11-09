69a523a5a4f2a505d24546935c78e1df796ce554
linzhenghui <1015583245@qq.com>
2020-11-09 21:02:42 +0800

    增加锁

--- jdkutil/src/main/java/com/delicacy/cookies/lock/MyLock.java
+++ jdkutil/src/main/java/com/delicacy/cookies/lock/MyLock.java
@@ -4,21 +4,32 @@ import sun.misc.Unsafe;
 
 import java.lang.reflect.Field;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.LockSupport;
 
 /**
- * todo 自闭锁，淦，要修改
- *
- * 遇到的问题
- * 1:锁不会释放，原因是cas时候没有将状态改变
- * 2:发现大概率的死锁的情况
- *
- *
  * 自定义锁，不可重入
- *
- *
+ * <p>
+ * <p>
+ * 初步建设：
+ * 1: cas修改变量
+ * 2: 循环获取
+ * 3: LockSupport挂起
+ * <p>
+ * <p>
+ * 遇到的问题：
+ * 1: 锁不会释放，原因是cas时候没有将状态改变
+ * 2: 发现大概率的死锁的情况，原因是存在线程存在重复入队列
+ * <p>
+ * <p>
+ * 优化：
+ * 1: 方法上加上final保证不被重写， 而且可能够给调用带来内联，提高效率
+ * 2：加上tryLock方法，能够保证超时获取锁失败
+ * <p>
  * 参考：
- * unsafe使用：https://tech.meituan.com/2019/02/14/talk-about-java-magic-class-unsafe.html
+ * final关键字的含义: https://zhuanlan.zhihu.com/p/60889552
+ * unsafe使用: https://tech.meituan.com/2019/02/14/talk-about-java-magic-class-unsafe.html
+ *
  * @author feigu
  * @date 2020/11/07
  */
@@ -41,6 +52,11 @@ public class MyLock {
     private int state = 0;
 
     /**
+     * state地址偏移量
+     */
+    private final long state_offset;
+
+    /**
      * 持有当前线程
      */
     private Thread currentThreadHolder;
@@ -48,35 +64,42 @@ public class MyLock {
     /**
      * 等待队列
      */
-    private ConcurrentLinkedQueue<Thread> queue = new ConcurrentLinkedQueue<>();
+    private final ConcurrentLinkedQueue<Thread> queue = new ConcurrentLinkedQueue<>();
 
     /**
      * Unsafe类为一单例实现，提供静态方法getUnsafe获取Unsafe实例，
      * 当且仅当调用getUnsafe方法的类为引导类加载器所加载时才合法，
      * 否则抛出SecurityException异常。
      */
-    private static final Unsafe unsafe;
+    private final Unsafe unsafe;
 
-    static {
+    {
+
         // 通过反射获取到unsafe类
         try {
+
+            // 加载unsafe类
             Field field = Unsafe.class.getDeclaredField("theUnsafe");
             field.setAccessible(true);
             unsafe = (Unsafe) field.get(null);
-        } catch (Exception ex){
-            throw new RuntimeException("myLock init error");
+
+            // 加载类偏移量
+            Field stateField = this.getClass().getDeclaredField("state");
+            state_offset = unsafe.objectFieldOffset(stateField);
+
+        } catch (Exception ex) {
+            throw new RuntimeException("myLock init error", ex);
         }
+
     }
 
     /**
      * cas原子比较
-      * @return
-     * @throws NoSuchFieldException
+     *
+     * @return
      */
-    private boolean stateCas(int origin, int expect) throws NoSuchFieldException {
-        Field field = this.getClass().getDeclaredField("state");
-        long offset = unsafe.objectFieldOffset(field);
-        return unsafe.compareAndSwapInt(this, offset, origin, expect);
+    private boolean stateCas(int origin, int expect) {
+        return unsafe.compareAndSwapInt(this, state_offset, origin, expect);
     }
 
     /**
@@ -84,12 +107,13 @@ public class MyLock {
      * 一开始就保证入队跟出队操作
      *
      * @return
-     * @throws NoSuchFieldException
      */
-    private boolean acquire() throws NoSuchFieldException {
+    private boolean acquire() {
         Thread currentThread = Thread.currentThread();
-        if(queue.isEmpty() || queue.peek().equals(currentThread)){
-            if(state == UN_LOCK_STATE && stateCas(UN_LOCK_STATE, LOCK_STATE)){
+        if (queue.isEmpty() || queue.peek().equals(currentThread)) {
+            if (state == UN_LOCK_STATE && stateCas(UN_LOCK_STATE, LOCK_STATE)) {
+                // 设置线程持有为当前线程
+                currentThreadHolder = currentThread;
                 return true;
             }
         }
@@ -99,59 +123,96 @@ public class MyLock {
 
     /**
      * 上锁
+     *
      * @return
      */
-    public void lock() throws NoSuchFieldException {
+    public final void lock() {
 
         Thread currentThread = Thread.currentThread();
 
         // 预先尝试获取
-        if(acquire()){
-            // 设置线程持有为当前线程
-            currentThreadHolder = currentThread;
+        if (acquire()) {
             return;
         }
 
-        // 入队，保证只入队一次
+        // 入队，有且仅有保证一次
         queue.add(currentThread);
 
-
-        for(;;){
-
-            if(acquire()){
-                // 设置线程持有为当前线程
-                currentThreadHolder = currentThread;
-
+        for (; ; ) {
+            if (acquire()) {
                 // 如果队列不为null，移除唤醒节点
-                if(!queue.isEmpty() && queue.peek().equals(currentThread)){
+                if (!queue.isEmpty() && queue.peek().equals(currentThread)) {
                     queue.poll();
                 }
                 return;
             }
-
             LockSupport.park();
         }
     }
 
+
+    public final boolean tryLock(long time, TimeUnit unit) {
+
+        // todo
+
+        // 预先尝试获取
+        if (acquire()) {
+            return true;
+        }
+
+        final Thread currentThread = Thread.currentThread();
+        final long delayTime = unit.toNanos(time);
+        final long deadLine = System.nanoTime() + delayTime;
+
+        // 入队，有且仅有保证一次
+        queue.add(currentThread);
+
+        for (; ; ) {
+
+            if (acquire()) {
+                // 如果队列不为null，移除唤醒节点
+                if (!queue.isEmpty() && queue.peek().equals(currentThread)) {
+                    queue.poll();
+                }
+                return true;
+            }
+
+            if(System.nanoTime() > deadLine){
+                return false;
+            }
+
+            LockSupport.parkNanos(delayTime);
+        }
+
+
+
+
+
+    }
+
+
+
+
+
     /**
      * 释放锁
+     *
      * @return
      */
-    public void unLock() throws NoSuchFieldException {
+    public final void unLock() {
         Thread currentThread = Thread.currentThread();
-        if(!currentThreadHolder.equals(currentThread)){
+        if (!currentThreadHolder.equals(currentThread)) {
             throw new RuntimeException("myLock unLock error currentThreadHolder is not current thread!");
         }
         // 修改状态
-        if(state == LOCK_STATE && stateCas(LOCK_STATE, UN_LOCK_STATE)){
+        if (state == LOCK_STATE && stateCas(LOCK_STATE, UN_LOCK_STATE)) {
             currentThreadHolder = null;
-            if(!queue.isEmpty()){
+            if (!queue.isEmpty()) {
                 Thread header = queue.peek();
                 LockSupport.unpark(header);
             }
         }
     }
-
 
 
 }
