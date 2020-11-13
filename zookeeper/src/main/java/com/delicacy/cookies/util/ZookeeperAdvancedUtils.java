492a5c2e811a2a0b0d65d06db3e047c24bb5e933
linzhenghui <1015583245@qq.com>
2020-11-13 14:27:49 +0800

    no message

---
+++ zookeeper/src/main/java/com/delicacy/cookies/util/ZookeeperAdvancedUtils.java
@@ -0,0 +1,16 @@
+package com.delicacy.cookies.util;
+
+/**
+ * zookeeper 缓存，分布式锁等高级特性
+ *
+ * 参考：
+ * 分布式锁实现（敖丙）：https://blog.csdn.net/qq_35190492/article/details/105352672
+ *
+ * @author linzhenghui
+ * @date 2020/11/13
+ */
+public class ZookeeperAdvancedUtils {
+    // todo
+
+
+}
