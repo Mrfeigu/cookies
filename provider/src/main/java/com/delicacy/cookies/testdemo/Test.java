b7428f32da9ff7bdcd3f20f42e8630e111f4cad2
linzhenghui <1015583245@qq.com>
2020-11-16 16:55:31 +0800

    增加zk锁Demo

--- provider/src/main/java/com/delicacy/cookies/testdemo/Test.java
+++ provider/src/main/java/com/delicacy/cookies/testdemo/Test.java
@@ -1,6 +1,8 @@
 package com.delicacy.cookies.testdemo;
 
 
+import java.text.MessageFormat;
+
 /**
  * @author linzhenghui
  * @date 2020/7/3
@@ -9,8 +11,10 @@ package com.delicacy.cookies.testdemo;
 public class Test {
 
     public static void main(String[] args) {
-        Jr jr = new JrKobe();
-        jr.beginSay();
+       String template = "{0}商品的兑换码为{1}，有效期{2}，请及时兑换，点击这里查看兑换流程";
+       Object[] params = {"京东购物卡100元", "京东卡号:JDV50240006333000091；密码： BDEB-F82A-FF3A-99F9", "无限"};
+        String format = MessageFormat.format(template, params);
+        System.out.println(format);
     }
 
 }
--- zookeeper/src/main/java/com/delicacy/cookies/util/ZookeeperAdvancedUtils.java
+++ zookeeper/src/main/java/com/delicacy/cookies/util/ZookeeperAdvancedUtils.java
@@ -1,16 +1,101 @@
 package com.delicacy.cookies.util;
 
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.locks.InterProcessLock;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
+import org.apache.curator.framework.recipes.locks.InterProcessReadWriteLock;
+import org.apache.curator.framework.recipes.locks.InterProcessSemaphoreMutex;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+
+import java.util.concurrent.TimeUnit;
+
 /**
  * zookeeper 缓存，分布式锁等高级特性
- *
+ * <p>
  * 参考：
  * 分布式锁实现（敖丙）：https://blog.csdn.net/qq_35190492/article/details/105352672
+ * Curator封装：https://www.cnblogs.com/qlqwjy/p/10518900.html
+ * 一个额外的zk知识：https://laijianfeng.org/2019/01/%E5%88%A9%E7%94%A8Zookeeper%E5%AE%9E%E7%8E%B0-%E5%88%86%E5%B8%83%E5%BC%8F%E9%94%81/
  *
  * @author linzhenghui
  * @date 2020/11/13
  */
 public class ZookeeperAdvancedUtils {
-    // todo
+
+    public static CuratorFramework client;
+
+    /** 排它，不可重入，可能会死锁*/
+    public final static InterProcessLock LOCK1;
+
+    /** 排它，可重入，重入几次就要释放几次*/
+    public final static InterProcessLock LOCK2;
+
+    /** 排它，可重入，重入几次就要释放几次，读写锁*/
+    public final static InterProcessReadWriteLock LOCK3;
+
+    /** 读锁*/
+    public final static InterProcessMutex READ_LOCK;
+
+    /** 写锁*/
+    public final static InterProcessMutex WRITE_LOCK;
+
+    static {
+        // 重试机制
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+        client = CuratorFrameworkFactory.builder()
+                .connectString("172.16.253.108:2181")
+                .sessionTimeoutMs(5000)
+                .connectionTimeoutMs(5000)
+                .retryPolicy(retryPolicy)
+                // 隔离命名
+                .namespace("lzh-test")
+                .build();
+        client.start();
+
+        LOCK1 = new InterProcessSemaphoreMutex(client, "/debug/myLock1");
+        LOCK2 = new InterProcessMutex(client,  "/debug/myLock2");
+        LOCK3 = new InterProcessReadWriteLock(client, "/debug/myLock3");
+
+        READ_LOCK = LOCK3.readLock();
+        WRITE_LOCK = LOCK3.writeLock();
+    }
+
+    /**
+     * InterProcessSemaphoreMutex  排它，不可重入，可能会死锁
+     * @throws Exception
+     */
+    public static void lock1Debug() throws Exception {
+        // 超时实现 lock.acquire(1L, TimeUnit.SECONDS);
+        LOCK1.acquire();
+        LOCK1.release();
+    }
+
+    /**
+     * InterProcessMutex 排它，可重入，入几次就要释放几次，说白了就是没用
+     * @throws Exception
+     */
+    public static void lock2Debug() throws Exception {
+        LOCK2.acquire();
+        LOCK2.release();
+    }
+
+    /**
+     * 读写锁，读读不冲突，其余冲突
+     * @throws Exception
+     */
+    public static void lock3Debug() throws Exception {
+        READ_LOCK.acquire();
+        READ_LOCK.release();
+
+        WRITE_LOCK.acquire();
+        WRITE_LOCK.release();
+    }
+
+    public static void main(String[] args) {
+
+    }
 
 
 }
