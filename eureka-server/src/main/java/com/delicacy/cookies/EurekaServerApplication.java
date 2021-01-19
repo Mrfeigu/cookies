5781d0efa3744bd44e71e3da42d9e35031c9f212
linzhenghui <1015583245@qq.com>
2021-01-19 12:28:58 +0800

    增加eureka-server模块

---
+++ eureka-server/src/main/java/com/delicacy/cookies/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.delicacy.cookies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author linzhenghui
+ * @date 2021/01/19
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
