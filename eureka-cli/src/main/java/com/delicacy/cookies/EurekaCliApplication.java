340b0495b0e409ea1b933184786daa7ff16eecb8
linzhenghui <1015583245@qq.com>
2021-01-19 22:07:05 +0800

    修改eureka客户端发现注解

--- eureka-cli/src/main/java/com/delicacy/cookies/EurekaCliApplication.java
+++ eureka-cli/src/main/java/com/delicacy/cookies/EurekaCliApplication.java
@@ -2,6 +2,7 @@ package com.delicacy.cookies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
@@ -9,7 +10,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  * @author linzhenghui
  * @date 2021/01/19
  */
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class EurekaCliApplication {
 
